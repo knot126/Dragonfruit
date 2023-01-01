8cc02cd457c333c6dfe23cfe0dbe690377b18a8c
knot126 <73200930+knot126@users.noreply.github.com>
2023-01-01 05:59:45 +0000

    Basic test

--- source/main.c
+++ source/main.c
@@ -1,6 +1,22 @@
 #include "util/log.h"
+#include "util/obfuscate.h"
+#include "util/string.h"
 
 int main(int argc, char *argv[]) {
-	DgLog(DG_LOG_INFO, "Hello, melon!");
+	DgLog(DG_LOG_INFO, "Dragonfruit v0.0.1");
+	
+	char buf[] = "This is a string of text that will be shifted using a Mediocre AB-compatible obfuscation algorithm.";
+	size_t len = DgStringLength(buf);
+	
+	DgLog(DG_LOG_INFO, "%s", buf);
+	
+	DgPolyalphabeticObfuscate("key", 0, len, buf);
+	
+	DgLog(DG_LOG_INFO, "%s", buf);
+	
+	DgPolyalphabeticDeobfuscate("key", 0, len, buf);
+	
+	DgLog(DG_LOG_INFO, "%s", buf);
+	
 	return 0;
 }
