cfe545c7b1ae8e82787fed96a6536e0afb0ce1bf
knot126 <73200930+knot126@users.noreply.github.com>
2023-01-21 20:53:07 +0000

    New

--- source/main.c
+++ source/main.c
@@ -1,9 +1,12 @@
 #include "util/log.h"
 #include "util/obfuscate.h"
 #include "util/string.h"
+#include "util/file.h"
 
-int main(int argc, char *argv[]) {
-	DgLog(DG_LOG_INFO, "Dragonfruit v0.0.1");
+void polyalphabetic(void) {
+	/**
+	 * Polyalphabetic
+	 */
 	
 	char buf[] = "This is a string of text that will be shifted using a Mediocre AB-compatible obfuscation algorithm.";
 	size_t len = DgStringLength(buf);
@@ -17,6 +20,26 @@ int main(int argc, char *argv[]) {
 	DgPolyalphabeticDeobfuscate("key", 0, len, buf);
 	
 	DgLog(DG_LOG_INFO, "%s", buf);
+}
+
+int main(int argc, char *argv[]) {
+	DgLog(DG_LOG_INFO, "Dragonfruit v0.0.1");
+	
+	char buf[] = "Encrypted using probably a shitty encryption algorithm..";
+	
+	DgLog(DG_LOG_VERBOSE, "%s", buf);
+	
+	DgObfuscateXorShiftShift("kekko", DgStringLength(buf), buf);
+	
+	DgLog(DG_LOG_VERBOSE, "%s", buf);
+	
+	DgFileSave(NULL, "./data.dat", DgStringLength(buf), buf);
+	
+	DgObfuscateXorShiftShift("kekko", DgStringLength(buf), buf);
+	
+	DgLog(DG_LOG_VERBOSE, "%s", buf);
+	
+	DgLog(DG_LOG_SUCCESS, "Done");
 	
 	return 0;
 }
---
+++ y
@@ -0,0 +1,4 @@
+[1;34mINFO: [0mDragonfruit v0.0.1
+[38;2;192;192;192mEncrypted using probably a shitty encryption algorithm..[0m
+[38;2;192;192;192mĞöç®èìıü¸zWfa®÷³¥¸ÖÙèÔÁ•N`\Gè'~*sS<K +,ëCÜPÚÒ MQLI2Ç[0m
+[38;2;192;192;192mEncrypted using probably a shitty encryption algorithm..[0m
